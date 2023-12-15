c5528b885fe3e121c13473cd4a6a7ebf29d95713
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-12-15 11:01:39 +0300

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Card, CardBody } from '@nextui-org/card';
+
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10"></section>
+    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 h-full">
+      <Card>
+        <CardBody></CardBody>
+      </Card>
+    </section>
   );
 }
